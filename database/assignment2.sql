df69727567518cb07920bb9aa4c35dde2a7d84e7
Nompilon <ngwenyanompilo@gmail.com>
2025-11-09 00:36:19 +0200

    ...

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,29 +1,40 @@
 -- 5.1 Insert Tony Stark
-INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
-VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
-
+INSERT INTO account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
 -- 5.2 Update Tony Stark’s account_type to "Admin"
 UPDATE account
 SET account_type = 'Admin'
 WHERE account_email = 'tony@starkent.com';
-
 -- 5.3 Delete Tony Stark
 DELETE FROM account
 WHERE account_email = 'tony@starkent.com';
-
 -- 5.4 Modify "GM Hummer" description using REPLACE
 UPDATE inventory
-SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
-WHERE inv_make = 'GM' AND inv_model = 'Hummer';
-
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_make = 'GM'
+    AND inv_model = 'Hummer';
 -- 5.5 Inner join to select make, model, classification name for "Sport"
-SELECT i.inv_make, i.inv_model, c.classification_name
+SELECT i.inv_make,
+    i.inv_model,
+    c.classification_name
 FROM inventory i
-INNER JOIN classification c ON i.classification_id = c.classification_id
+    INNER JOIN classification c ON i.classification_id = c.classification_id
 WHERE c.classification_name = 'Sport';
-
 -- 5.6 Update all records in the inventory table 
 UPDATE inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
-
